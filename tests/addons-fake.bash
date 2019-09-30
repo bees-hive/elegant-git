56438202cbc458e90b279adb82d51b567ad1a2f2
Dmytro Serdiuk <dmytro.serdiuk@gmail.com>
2019-09-30 11:17:13 +0300

    Create fake executable prior intitiating fake command
    The change simplifies the analysis of faild tests as the first log lines
    always will be a fake executable creation, and other lines will be about
    creation of fake commands.

--- tests/addons-fake.bash
+++ tests/addons-fake.bash
@@ -21,17 +21,9 @@ fake() {
     FIXTURE_HOME="$PROGRAM_PATH/$(echo "$2" | sed 's/[^0-9a-zA-Z]*//g')"
     MOCK="$MOCK_DIR/$BASENAME"
 
-    [[ -d "$FIXTURE_HOME" ]] && rm -r "$FIXTURE_HOME"
-    echo ""
-    echo "==>> Creating mock: ${FIXTURE_HOME}"
-    _ex_fake mkdir -p "$FIXTURE_HOME"
-    echo -e "$3" | tee -i "$FIXTURE_HOME/exit_code"
-    echo -e "$4" | tee -i "$FIXTURE_HOME/stdout"
-    echo -e "$5" | tee -i "$FIXTURE_HOME/stderr"
-
     if [[ ! -e "${MOCK}" ]]; then
         echo ""
-        echo "==>> Creating executable: ${MOCK}"
+        echo "==>> Creating fake executable: ${MOCK}"
         ORIGIN_BINARY=$(which ${BASENAME})
         cat <<MOCK | tee -i ${MOCK} && _ex_fake chmod +x "$MOCK"
 #!/usr/bin/env bash
@@ -49,6 +41,14 @@ else
 fi
 MOCK
     fi
+
+    [[ -d "$FIXTURE_HOME" ]] && rm -r "$FIXTURE_HOME"
+    echo ""
+    echo "==>> Creating fake command: ${FIXTURE_HOME}"
+    _ex_fake mkdir -p "$FIXTURE_HOME"
+    echo -e "$3" | tee -i "$FIXTURE_HOME/exit_code"
+    echo -e "$4" | tee -i "$FIXTURE_HOME/stdout"
+    echo -e "$5" | tee -i "$FIXTURE_HOME/stderr"
 }
 
 fake-pass() {
