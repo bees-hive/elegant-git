b30e2a9c0b98d34449640d61c1f3f2de92a69ef3
Dmytro Serdiuk <dmytro.serdiuk@gmail.com>
2019-10-08 17:25:03 +0300

    Add Bash completion for `obtain-work` command
    `obtain-work` completion will propose fetched remote branches as the
    candidates.

--- completions/git-elegant.bash
+++ completions/git-elegant.bash
@@ -15,7 +15,7 @@ _git_elegant() {
             local data=$(git branch | awk -F ' +' '! /\(no branch\)/ {print $2}')
             COMPREPLY=( $(compgen -W "${data}" ${cur}) )
             return 0 ;;
-        accept-work)
+        accept-work|obtain-work)
             COMPREPLY=(
                 $(compgen -W "$(git branch --remotes --list)" -- ${cur})
             )
