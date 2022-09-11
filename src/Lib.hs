ea55ea15f2669fcaceb2d8c60a0d235a24790f36
teggotic <teggot.teggot@gmail.com>
2022-09-12 00:50:21 +0300

    Improve test suite structure
    Make it easier to test separate modules.
    
    The idea now is that to test a module you should duplicate the directory
    structure and add the `Spec` suffix. You should create a top-level
    `spec`.
    You should look at `LibSpec.hs` for example setup.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,12 @@
 module Lib
     ( someFunc
+    , tst
     ) where
 
 import Universum
 
 someFunc :: IO ()
 someFunc = putTextLn "someFunc"
+
+tst :: Int
+tst = 1
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,1 @@
-import Universum
-import qualified Universum.Unsafe as Unsafe
-import Test.Hspec
-
-main :: IO ()
-main = hspec $ do
-  describe "Prelude.head" $ do
-    it "returns the first element of a list" $ do
-      Unsafe.head [23 ..] `shouldBe` (23 :: Int)
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
