a193abbd69a4273beda608e563b82f51941ecf36
teggotic <teggot.teggot@gmail.com>
2022-12-18 20:42:38 +0200

    Encapsulate show-work command logic in one module
    Haskell does not have a way to create multiline strings so we use the
    `string-qq` package and haskell's metaprogramming `QuasiQuotes`.
    
    I'm not sure if this is the best way to encapsulate the logic in one
    module, but we can change this anytime in the future fairly easy.
    
    #285

--- src/Elegit/Cli/Parser.hs
+++ src/Elegit/Cli/Parser.hs
@@ -1,6 +1,7 @@
 module Elegit.Cli.Parser where
 
-import           Elegit.Cli.Command  (ElegitCommand (ShowWorkCommand))
+import qualified Elegit.Cli.Action.ShowWork as ShowWork
+import           Elegit.Cli.Command
 import           Options.Applicative
 import           Universum
 
@@ -8,16 +9,10 @@ import           Universum
 type Command a = Mod CommandFields a
 
 
-showWorkCommand :: Command ElegitCommand
-showWorkCommand =
-    command "show-work" $
-        flip info (progDesc "Prints HEAD state.") $ pure ShowWorkCommand
-
-
 dayToDayContributionsCommand :: Command ElegitCommand
 dayToDayContributionsCommand =
-    commandGroup " make day-to-day contributions"
-    <> showWorkCommand
+    commandGroup "make day-to-day contributions"
+    <> ShowWork.cli
 
 
 cli :: ParserInfo ElegitCommand
