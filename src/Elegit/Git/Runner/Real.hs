8e7388eaafd836371fc42888baee5438fb949e75
teggotic <teggot.teggot@gmail.com>
2023-02-15 02:51:36 +0200

    Improve testability of the `Real` interpreter
    There is an issue in how we test actions. Fundamentally, we rely on the
    fact that the `Real` runner is generating `git` calls correctly. This
    could be neglected in theory, but there is another issue, which forces
    us to generate "textual" representation of what our command is doing, in
    multiple places, which leads to code repetition.
    As a solution to this, I've created 1 data type for every type of the
    command we can execute, and implemeted the way to render it to the
    "textual" representation.
    This could be used by `Real` runner directly, but this still suffers
    from a different issue. We give `Real` runner to much access, by adding
    `MonadIO` to the context.
    There is a solution however. We can create a custom `Monad` which can
    provide all needed functions for `Real` to proceed. Going this way, we
    can hide how we "render" and "execute" git calls from the `Real` runner,
    which implies that it has no way to run arbitrary commands at all.
    
    #299

--- src/Elegit/Git/Runner/Real.hs
+++ src/Elegit/Git/Runner/Real.hs
@@ -3,12 +3,89 @@ module Elegit.Git.Runner.Real where
 import           Control.Exception.Safe    (throwString)
 import           Control.Monad.Free.Church
 import           Control.Monad.HT
-import           Data.Text                 (stripEnd)
 import qualified Elegit.Git.Action         as GA
+import           Elegit.Git.Exec           (GitCommand (..), MonadGitExec (execGit, gLine, pText, pTextLn))
 import           Fmt
-import           System.IO                 (hFlush)
-import           System.Process.Typed      (ExitCode (ExitFailure, ExitSuccess), proc, readProcess)
 import           Universum                 as U
+
+
+-- | Execute the action in the real world.
+executeGit :: (MonadCatch m, MonadGitExec m) => GA.FreeGit () -> m ()
+executeGit = foldF executeGitF
+
+
+-- | Interpreter for the real world
+--
+-- Currently just prints the resulting commands without any execution.
+--
+executeGitF :: (MonadCatch m, MonadGitExec m) => GA.GitF a -> m a
+executeGitF arg = case arg of
+  GA.CurrentBranch gc next -> do
+    mCurrentBranch <- execGit (GCCB gc)
+    case mCurrentBranch of
+      Nothing -> throwString "No branch found. Seems like this repository was not initialized fully."
+      Just currentBranch ->
+        return $ next currentBranch
+
+  GA.BranchUpstream gc next -> do
+    mUpstreamBranch <- execGit (GCBU gc)
+    return $ next mUpstreamBranch
+
+  GA.Log gc next -> do
+    logs <- lines . fromMaybe "" <$> execGit (GCL gc)
+    return $ next logs
+  GA.Status gc next -> do
+    changes <- lines . fromMaybe "" <$> execGit (GCS gc)
+    return $ next changes
+  GA.StashList gc next -> do
+    stashes <- lines . fromMaybe "" <$> execGit (GCSL gc)
+    return $ next stashes
+
+  GA.AliasesToRemove gc next -> do
+    oldAliasesM <- execGit (GCATR gc)
+    return $ next (oldAliasesM >>= nonEmpty . lines)
+  GA.ReadConfig gc next -> do
+    next <$> execGit (GCRC gc)
+  GA.SetConfig gc next -> do
+    U.void $ execGit (GCSC gc)
+    return next
+  GA.UnsetConfig gc next -> do
+    U.void $ execGit (GCUC gc)
+    return next
+  GA.Prompt prompt pDefaultM next -> do
+    let
+      askPrompt = do
+        pText (colored Purple Normal message)
+        gLine
+
+      message :: Text
+      message =
+        case pDefaultM of
+          Just pDefault -> fmt ""+|prompt|+" {"+|pDefault|+"}: "
+          Nothing       -> fmt ""+|prompt|+": "
+
+    answer <-
+      case pDefaultM of
+        Nothing -> until (not . null) askPrompt
+        Just pDefault -> do
+          answer <- askPrompt
+          if null answer
+             then return pDefault
+             else return answer
+    return $ next answer
+
+  GA.FormatInfo content next -> do
+    return $ next $ colored Green Normal content
+  GA.FormatCommand content next -> do
+    return $ next $ colored Green Normal "==>>" <> " " <> colored Blue Bold content
+
+  GA.PrintText content next -> do
+    pTextLn content
+    return next
+
+colored :: Color -> FontStyle -> Text -> Text
+colored color style content =
+  fmt "\x1b["+||fontStyleCode style||+";"+||colorCode color||+"m"+|content|+"\x1b[0m"
 
 
 data Color
@@ -29,129 +106,3 @@ data FontStyle
 fontStyleCode :: FontStyle -> Int
 fontStyleCode Normal = 0
 fontStyleCode Bold   = 1
-
-
-runGit :: (MonadIO m) => Text -> m (Maybe Text)
-runGit cmd = do
-    (eCode, outputBS, _errBS) <- readProcess $ proc "git" (toString <$> words cmd)
-    case eCode of
-      ExitFailure _ -> do
-          -- TODO: Know what error is expected and log unexpected ones.
-          return Nothing
-      ExitSuccess -> do
-          let output = stripEnd $ decodeUtf8 outputBS
-          return $ if null output then Nothing else Just output
-
--- | Execute the action in the real world.
-executeGit :: (MonadCatch m, MonadIO m) => GA.FreeGit () -> m ()
-executeGit = foldF executeGitF
-
-
-colored :: Color -> FontStyle -> Text -> Text
-colored color style content =
-    fmt "\x1b["+||fontStyleCode style||+";"+||colorCode color||+"m"+|content|+"\x1b[0m"
-
--- | Interpreter for the real world
---
--- Currently just prints the resulting commands without any execution.
---
-executeGitF :: (MonadCatch m, MonadIO m) => GA.GitF a -> m a
-executeGitF arg = case arg of
-    GA.CurrentBranch next -> do
-        mCurrentBranch <- runGit "rev-parse --abbrev-ref @"
-        case mCurrentBranch of
-          Nothing -> throwString "No branch found. Seems like this repository was not initialized fully."
-          Just currentBranch ->
-            return $ next currentBranch
-
-    GA.BranchUpstream branch next -> do
-        mUpstreamBranch <- runGit (fmt "rev-parse --abbrev-ref "+|branch|+"@{upstream}")
-        return $ next mUpstreamBranch
-
-    GA.Log lType base target next -> do
-        let
-            logArg :: Text
-            logArg = case lType of
-                       GA.LogOneLine -> "--oneline"
-
-        logs <- lines . fromMaybe "" <$> runGit (fmt "-c color.ui=always log "+|logArg|+" "+|base|+".."+|target|+"")
-        return $ next logs
-    GA.Status sType next -> do
-        let
-            statusFormat :: Text
-            statusFormat = case sType of
-                             GA.StatusShort -> "--short"
-
-        changes <- lines . fromMaybe "" <$> runGit (fmt "-c color.status=always status "+|statusFormat|+"")
-        return $ next changes
-    GA.StashList next -> do
-        stashes <- lines . fromMaybe "" <$> runGit "stash list"
-        return $ next stashes
-
-    GA.AliasesToRemove cScope next -> do
-        let
-            scope :: Text
-            scope = case cScope of
-                      GA.LocalConfig  -> "--local"
-                      GA.GlobalConfig -> "--global"
-                      GA.AutoConfig   -> ""
-
-        oldAliasesM <- runGit (fmt "config "+|scope|+" --name-only --get-regexp \"^alias.\" \"^elegant ([-a-z]+)$\"")
-        return $ next (oldAliasesM >>= nonEmpty . lines)
-    GA.ReadConfig cScope cName next -> do
-        let
-            scope :: Text
-            scope = case cScope of
-                      GA.LocalConfig  -> "--local"
-                      GA.GlobalConfig -> "--global"
-                      GA.AutoConfig   -> ""
-
-        next <$> runGit (fmt "config "+|scope|+" --get "+|cName|+"")
-    GA.SetConfig cScope cName cValue next -> do
-        let
-            scope :: Text
-            scope = case cScope of
-                      GA.LocalConfig  -> "--local"
-                      GA.AutoConfig   -> "--local"
-                      GA.GlobalConfig -> "--global"
-        U.void $ runGit (fmt "config "+|scope|+" "+|cName|+" "+|cValue|+"")
-        return next
-    GA.UnsetConfig cScope cName next -> do
-        let
-            scope :: Text
-            scope = case cScope of
-                      GA.LocalConfig  -> "--local"
-                      GA.AutoConfig   -> "--local"
-                      GA.GlobalConfig -> "--global"
-        U.void $ runGit (fmt "config "+|scope|+" --unset "+|cName|+"")
-        return next
-    GA.Prompt prompt pDefaultM next -> do
-        let
-            askPrompt = do
-                putText (colored Purple Normal message)
-                liftIO $ hFlush stdout
-                getLine
-
-            message :: Text
-            message =
-                case pDefaultM of
-                  Just pDefault -> fmt ""+|prompt|+" {"+|pDefault|+"}: "
-                  Nothing       -> fmt ""+|prompt|+": "
-
-        answer <- case pDefaultM of
-              Nothing -> until (not . null) askPrompt
-              Just pDefault -> do
-                  answer <- askPrompt
-                  if null answer
-                     then return pDefault
-                     else return answer
-        return $ next answer
-
-    GA.FormatInfo content next -> do
-        return $ next $ colored Green Normal content
-    GA.FormatCommand content next -> do
-        return $ next $ colored Green Normal "==>>" <> " " <> colored Blue Bold content
-
-    GA.PrintText content next -> do
-        putTextLn content
-        return next
--- src/Lib.hs
+++ src/Lib.hs
@@ -4,6 +4,7 @@ import qualified Elegit.Cli.Action.AcquireRepository as AcquireRepository
 import qualified Elegit.Cli.Action.ShowWork          as ShowWork
 import           Elegit.Cli.Command                  (ElegitCommand (..))
 import qualified Elegit.Cli.Parser                   as P
+import           Elegit.Git.Exec                     (GitExecT (runGitExecT))
 import           Elegit.Git.Runner.Real              (executeGit)
 import           Options.Applicative                 (customExecParser)
 import           Universum
@@ -13,6 +14,7 @@ runCli = do
   cmd <- liftIO $ customExecParser P.cliPrefs P.cli
 
   flip catch (\e -> putTextLn $ "Caught exception: " <> show (e :: SomeException) ) $
+    runGitExecT $
     executeGit $
       case cmd of
          ShowWorkCommand          -> ShowWork.cmd
